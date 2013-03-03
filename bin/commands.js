15e26ba2ab767c3dd35829d9e542051cecbf8d2e
James Halliday <mail@substack.net>
2013-03-03 06:22:23 -0800

    command works again

--- bin/commands.js
+++ bin/commands.js
@@ -3,130 +3,26 @@ var fs = require('fs');
 var path = require('path');
 var normalizeKey = require('../lib/normalize_key');
 
-var config = require('./config');
 var VERSION = require('../package.json').version;
 
-exports.list = function (st, hub) {
-    var read = st.read(hub);
-    st.on('keys', function (keys) {
-        var names = Object.keys(keys)
-            .map(function (key) { return keys[key] })
-            .filter(function (r) {
-                return r.connections > 0 && r.write;
-            })
-            .map(function (r) { return r.name })
-        ;
-        if (names.length) console.log(names.join('\n'));
-        read.end();
-    });
+exports.duplex = exports[''] = function (st, hub, argv) {
+    var ds = st.createStream(hub);
+    process.stdin.pipe(ds);
+    process.stdin.pipe(process.stdout);
+    process.stdin.resume();
 };
 
-exports.write = exports[''] = function (st, hub, argv) {
-    var ws = st.write(hub);
+exports.write = function (st, hub, argv) {
+    var ws = st.createWriteStream(hub);
     process.stdin.pipe(ws);
     process.stdin.pipe(process.stdout);
     process.stdin.resume();
 };
 
-exports.names = function (st, hub) {
-    var read = st.read(hub, function (err, keys) {
-        if (err) return showError(err);
-        
-        var names = Object.keys(keys).reduce(function (acc, key) {
-            acc[keys[key].name] = true;
-            return acc;
-        }, {});
-        names = Object.keys(names);
-        
-        if (names.length) console.log(names.join('\n'));
-        read.end();
-    });
-};
-
 exports.read = function (st, hub, argv) {
     var name = argv._[0];
-    var read = st.read(hub);
-    read(name).pipe(process.stdout);
-};
-
-exports.config = function (st, hub, argv) {
-    var c = config(argv);
-    var cmd = argv._.shift();
-    
-    if (cmd === 'get') {
-        var key = argv._[0];
-        return c.get(key, function (err, value) {
-            if (err) showError(err)
-            else console.log(value)
-        });
-    }
-    
-    if (cmd === 'set') {
-        var key = argv._[0];
-        var value = argv._[1];
-        return c.set(key, value, function (err) {
-            if (err) showError(err);
-        });
-    }
-    
-    if (cmd === 'remove' || cmd === 'rm') {
-        var key = argv._[0];
-        return c.remove(key, function (err) {
-            if (err) showError(err);
-        });
-    }
-    
-    if (cmd === 'list' || cmd === 'ls') {
-        return c.loadFile(function (err, cfg) {
-            if (err) return showError(err);
-            
-            console.log('{\n' + Object.keys(cfg).map(function (key) {
-                if (key === 'keys') return '  "keys": [ ... ]';
-                var value = JSON.stringify(cfg[key]);
-                return '  ' + JSON.stringify(key) + ': ' + value;
-            }).join(',\n') + '\n}');
-        });
-    }
-    
-    if (cmd === 'file') {
-        return console.log(c.getFile());
-    }
-    
-    console.log([
-        'usage: ',
-        '',
-        '  straggler config get KEY',
-        '  straggler config set KEY VALUE',
-        '  straggler config remove KEY',
-        '  straggler config list',
-        ''
-    ].join('\n'));
-};
-
-exports.profiles = function (st, hub, argv) {
-    var c = config(argv);
-    var dir = path.dirname(c.getFile());
-    var files = fs.readdirSync(dir)
-        .filter(function (file) {
-            return /\.json$/.test(file)
-        })
-        .map(function (file) {
-            return path.basename(file).replace(/\.json$/, '')
-        })
-    ;
-    if (files.length) console.log(files.join('\n'));
-};
-
-exports.entry = function (st, hub, argv) {
-    var c = config(argv);
-    c.load(function (err, cfg) {
-        console.log(JSON.stringify({
-            key: normalizeKey(cfg.keys.public),
-            name: cfg.name || c.getProfile(),
-            read: Boolean(argv.r || argv.read),
-            write: Boolean(argv.w || argv.write),
-        }, null, 2));
-    });
+    var rs = st.createReadStream(hub);
+    rs.pipe(process.stdout);
 };
 
 exports.hub = function (st, hub, argv) {
--- bin/script.js
+++ bin/script.js
@@ -1,10 +1,86 @@
 #!/usr/bin/env node
 var straggler = require('../');
+var http = require('http');
 var fs = require('fs');
-var argv = require('optimist').argv;
+var createKeys = require('rsa-json');
 
-var config = require('./config')(argv);
-var commands = require('./commands');
+var normalizeKey = require('../lib/normalize_key');
+var VERSION = require('../package.json').version;
+
+var argv = require('optimist')
+    .boolean('h', 'help', 'g', 'generate')
+    .argv
+;
+
+if (argv.h || argv.help) return showUsage(0);
+
+if (argv.g || argv.generate) {
+    return createKeys(function (err, pair) {
+        if (err) return showError(err);
+        console.log(JSON.stringify(pair, null, 2));
+    });
+}
+
+if (!argv.k && !argv.keys) {
+    return showError([
+        'Specify a keypair file with -k',
+        'Generate a keypair file with -g.'
+    ].join('\n'));
+}
+
+var keys = JSON.parse(fs.readFileSync(argv.k || argv.keys));
+var st = straggler(keys);
+
+if ((argv.r && argv.w) || argv.rw) {
+    var ds = st.createStream(argv.r || argv.w || argv.rw);
+    process.stdin.pipe(ds);
+    process.stdin.pipe(process.stdout);
+    process.stdin.resume();
+    return;
+}
+
+if (argv.w) {
+    var ws = st.createWriteStream(argv.w);
+    process.stdin.pipe(ws);
+    process.stdin.pipe(process.stdout);
+    process.stdin.resume();
+    return;
+}
+
+if (argv.r) {
+    var rs = st.createReadStream(argv.r);
+    rs.pipe(process.stdout);
+    return;
+}
+
+if (argv.l || argv.listen) {
+    var port = argv.l || argv.listen;
+    var file = argv.a || argv.authorized;
+    
+    if (!file) {
+        return showError([
+            'usage: straggler -l PORT',
+            '-k keys.json -a authorized.json'
+        ].join(' '));
+    }
+    
+    var authorized = JSON.parse(fs.readFileSync(file));
+    var hub = st.createHub(authorized);
+    
+    var server = http.createServer(function (req, res) {
+        if (hub.test(req.url)) return hub.handle(req, res);
+        res.end('straggler version ' + VERSION);
+    });
+    server.listen(port);
+    return;
+}
+
+return showUsage(1);
+
+function showError (err) {
+    console.error(String(err));
+    process.exit(1);
+}
 
 function showUsage (code) {
     var s = fs.createReadStream(__dirname + '/usage.txt');
@@ -13,31 +89,3 @@ function showUsage (code) {
         process.exit(code);
     });
 }
-
-var cmd = argv._.shift() || '';
-if (cmd === 'help' || argv.help || argv.h) return showUsage(0);
-var hubRequired = [ 'write', '', 'list', 'read' ];
-
-config.load(function (err, cfg) {
-    if (err) {
-        console.error(String(err));
-        return process.exit(1);
-    }
-    if (!cfg.hubs) cfg.hubs = {};
-    
-    var hub = /:/.test(cfg.hub)
-        ? cfg.hub
-        : cfg.hubs && cfg.hubs[cfg.hub]
-    ;
-    if (!hub) hub = process.env.STRAGGLER_HUB || cfg.hubs.default;
-    if (!hub && hubRequired.indexOf(cmd) >= 0) {
-        console.error('--hub not specified and no default hub configured.');
-        return process.exit(1);
-    }
-    
-    var fn = commands[cmd];
-    if (!fn) return showUsage(1);
-    
-    var st = straggler(cfg.keys);
-    fn(st, hub, argv);
-});
